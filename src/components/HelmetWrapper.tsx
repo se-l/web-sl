e52843931e9553325add09c048fdce22b24e5651
Sebastian Lueneburg <se-l@gmx.de>
2024-07-28 08:10:10 +0800

    rm console

--- src/components/HelmetWrapper.tsx
+++ src/components/HelmetWrapper.tsx
@@ -8,33 +8,32 @@ interface IHelmetWrapperProps {
     children: any
 }
 
-export default function HelmetWrapper({notebook, children}: IHelmetWrapperProps) {
+export default function HelmetWrapper({ notebook, children }: IHelmetWrapperProps) {
     const [helmets, setHelmets] = useState<any[]>([])
 
     useEffect(() => {
-        console.log('Running Helmet again')
         const helmetsTmp: any[] = []
         //@ts-ignore
         const scriptElements = getScriptElements(parse(notebook))
         scriptElements.forEach((el: React.ReactElement<any, any>, i: number) => {
-        var s = document.createElement('script')
-        if (el.props.type !== undefined) {
-            s.type = el.props.type
-        }
-        if (el.props.src !== undefined) s.src = el.props.src
-        if (el.props.dangerouslySetInnerHTML !== undefined) s.text = el.props.dangerouslySetInnerHTML.__html
-        helmetsTmp.push(<script type={s.type} key={i}>{s.text}</script>);
+            var s = document.createElement('script')
+            if (el.props.type !== undefined) {
+                s.type = el.props.type
+            }
+            if (el.props.src !== undefined) s.src = el.props.src
+            if (el.props.dangerouslySetInnerHTML !== undefined) s.text = el.props.dangerouslySetInnerHTML.__html
+            helmetsTmp.push(<script type={s.type} key={i}>{s.text}</script>);
         });
         setHelmets(helmetsTmp)
-        
+
     }, [notebook])
 
-  return (
-    <HelmetProvider>
-        <Helmet>
-            {helmets}
-        </Helmet>
-        {children}
-    </HelmetProvider>
-  )
+    return (
+        <HelmetProvider>
+            <Helmet>
+                {helmets}
+            </Helmet>
+            {children}
+        </HelmetProvider>
+    )
 }
